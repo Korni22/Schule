0734f218a3d2dfb2474f721f895e7b1e190dd6ef
Eric S. <vollkorngangsta@googlemail.com>
2013-04-15 13:33:22 +0200

    Create zahlen_sortieren.cpp

---
+++ Programmieren/zahlen_sortieren.cpp
@@ -0,0 +1,22 @@
+Include <iostream>
+using namespace std;
+
+bool mean(){
+
+  int SE = 0;
+
+	cout << "Hallo" << endl << "Dieses Programm listet 4 Zahlen deren Größe nach auf" << endl
+		 << "1. Starten" << endl "2. Beenden";
+	cin >> abfrage;
+
+	if (abfrage == 1)(
+		return true
+		)
+
+	else if (abfrage == 2)(
+	exit();
+	)
+
+	else(
+		menu();
+	))
