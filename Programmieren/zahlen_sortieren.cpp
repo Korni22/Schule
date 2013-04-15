cd288d2047b464b3557e75ca865ed8be1532fe63
PlaceYourBets <max_riedl@hotmail.de>
2013-04-15 14:02:37 +0200

    Update zahlen_sortieren.cpp

--- Programmieren/zahlen_sortieren.cpp
+++ Programmieren/zahlen_sortieren.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //Globale Variablen
-long numOne, numTwo, numThree, numFour;
+long numOne, numTwo, numThree, numFour, temp1, temp2;
 
 // Das Hauptmenu
 bool menu(){
@@ -10,32 +10,61 @@ bool menu(){
 int SE = 0;
 
 cout << "Hallo!" << endl << "Dieses Programm listet Ihnen 4 Zahlen in aufsteigender Reihenfolge auf" << endl
-<< "1 = Start" << endl << "2 = Beenden";
+   << "1 = Start" << endl << "2 = Beenden";
 cin >> SE;
 
 if (SE == 1){
-return true;
+	return true;
 }
 else if (SE == 2){
-return false;
+	return false;
 }
 else{
-return false;
-menu();
+	return false;
+	menu();
 }}
 
 // Zahlen eingeben und speichern
-getNumbers(){
-cout << "Geben Sie nun 4 Zahlen ein" << endl;
+bool getNumbers(){
+	cout << "Geben Sie nun 4 Zahlen ein" << endl;
+	cout << "Zahl 1:" << endl;
+	cin >> numOne;
+	cout << "Zahl 2:" << endl;
+	cin >> numTwo;
+	cout << "Zahl 3:" << endl;
+	cin >> numThree;
+	cout << "Zahl 4:" << endl;
+	cin >> numFour;
+	return true;
 }
 
-// main beginnt
+// Sortieren der eingegebenen Zahlen
+bool sortNumbers(){
+	if(numOne < numTwo){
+		numbOne = temp1;
+		numbTwo = temp2;
+		numbOne = temp2;
+		numbTwo = temp1;
+
+	}
+
+	return true;
+}
+
+
+
 int main(){
 
-if(menu()){
-getNumbers();
-}
-else {
-exit();
-}
+	if(menu()){
+		getNumbers();
+		sortNumbers();
+	}
+	else {
+	}
+
+
+
+
+
+
 };
