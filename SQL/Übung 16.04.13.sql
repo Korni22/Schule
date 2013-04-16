88e83ebfdc530e5e342a38f98466bf39b7dade55
Eric S. <vollkorngangsta@googlemail.com>
2013-04-16 10:07:36 +0200

    Create Übung 16.04.13.sql

---
+++ SQL/Übung 16.04.13.sql
@@ -0,0 +1,23 @@
+--Übung(SQL-2 : DDL u. DML)
+USE DbLKapxx; --Übungsdatenbabk auswählen
+
+--*************************************************************************
+--Übungsaufgabe 1 
+--Formulieren Sie zu den folgenden Aufgabenstellung die SQL-II-Statements 
+--Erzeugen Sie die Tabelle Artikel mit den Attributen Nr, Name , Anzahl
+--Fügen Sie in diese Tabelle folgende Datensätze ein 
+--100 , AAA , 10
+--110 , BBB , 20
+--120 , CCC,   5
+
+--Ändern Sie die Anzahl 20 auf 25
+
+--Fügen Sie die Spalte Preis ein
+
+--Füllen Sie Spalte Preis mit :  Nr 100 – 5.00
+--Nr 110 – 10.00 
+--Nr 120 – 20.00
+
+--Erhöhen Sie alle Preise um 10%
+
+--Löschen Sie die Datensätze mit Nr <120
