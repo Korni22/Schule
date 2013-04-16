ea0380fed519267dd685ddff53c595ef0a99ee15
Eric S. <vollkorngangsta@googlemail.com>
2013-04-16 10:24:39 +0200

    loift.

--- SQL/16.04.13_Eric_übung.sql
+++ SQL/16.04.13_Eric_übung.sql
@@ -1,7 +1,16 @@
 --Übung(SQL-2 : DDL u. DML)
-USE DbLKapxx; --Übungsdatenbabk auswählen
+USE DbLKap09; --Übungsdatenbabk auswählen
 
---*************************************************************************
+DROP TABLE tbl_07uebung;
+
+--Tabelle Personal anlegen
+CREATE TABLE tbl_07buebung
+(
+--Attributbezeichner -  Datentyp  - Funktion
+   nr                INTEGER     PRIMARY KEY,
+   name              Char(3)     NOT NULL,
+   anzahl            Integer     NOT NULL,
+);
 --Übungsaufgabe 1 
 --Formulieren Sie zu den folgenden Aufgabenstellung die SQL-II-Statements 
 --Erzeugen Sie die Tabelle Artikel mit den Attributen Nr, Name , Anzahl
@@ -9,6 +18,15 @@ USE DbLKapxx; --Übungsdatenbabk auswählen
 --100 , AAA , 10
 --110 , BBB , 20
 --120 , CCC,   5
+
+Insert INTO tbl_07buebung
+VALUES(100, 'AAA', 10);
+
+Insert INTO tbl_07buebung
+VALUES(110, 'BBB', 20);
+
+Insert INTO tbl_07buebung
+VALUES(120, 'CCC', 5);
 
 --Ändern Sie die Anzahl 20 auf 25
 
