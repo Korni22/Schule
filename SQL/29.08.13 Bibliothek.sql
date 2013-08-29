735fedd5bb7cc9a63c310df7f2c7842ae83b216a
Eric S. <Korni22@users.noreply.github.com>
2013-08-29 09:08:46 +0200

    Update 29.08.13 Bibliothek.sql

--- SQL/29.08.13 Bibliothek.sql
+++ SQL/29.08.13 Bibliothek.sql
@@ -2,15 +2,6 @@ USE dblkap09
 
 CREATE TABLE tbl_07bautor
 
-(
---Attributbezeichner -  Datentyp  - Funktion
-   autornr              INTEGER     PRIMARY KEY,
-   Name                 CHAR(35)    NOT NULL,
-   vorname              CHAR(35),
-  geboren               DATE,
-);
-
-
 CREATE TABLE tbl_ueArtikel07b_Buchexemplar(
 ExNr  INTEGER     PRIMARY KEY IDENTITY(100,10),
 ISBN INTEGER,
