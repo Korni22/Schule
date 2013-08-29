4f3ccc819662826a3f532a7421476216b2b33a25
Imhotep88 <Marc.Koehlers@t-online.de>
2013-08-29 00:06:34 -0700

    Update 29.08.13 Bibliothek.sql

--- SQL/29.08.13 Bibliothek.sql
+++ SQL/29.08.13 Bibliothek.sql
@@ -17,13 +17,10 @@ ISBN INTEGER,
 Standort Char(15),
 );
 
-CREATE TABLE tbl_ueArtikel07b_Autor(
-AutorNr     INTEGER     PRIMARY KEY IDENTITY(100,10),
-Name        INTEGER,
-Vorname     Char(15),
-GebDatum    DATE,
-TodDatum    DATE,
-Werke       Char (100),
-Beruf       Char (20),
-Wohnort     Char (30),
+CREATE Table tbl_07b_ausleihvorgang(
+AusleihNr INTEGER PRIMARY KEY,
+KundenNr INTEGER FOREIGN KEY,
+ExNr INTEGER FOREING KEY,
+Ausleihdatum DATE,
+Rückgabedatum DATE,
 );
