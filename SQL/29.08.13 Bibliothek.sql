89b0ecb5fd087daebd5191df732930d5b7815289
Eric S. <Korni22@users.noreply.github.com>
2013-08-29 09:07:27 +0200

    Update 29.08.13 Bibliothek.sql

--- SQL/29.08.13 Bibliothek.sql
+++ SQL/29.08.13 Bibliothek.sql
@@ -28,6 +28,17 @@ Beruf       Char (20),
 Wohnort     Char (30),
 );
 
+CREATE TABLE tbl_ueArtikel07b_BuchtypAutor(
+AutorNr     INTEGER     PRIMARY KEY IDENTITY(100,10),
+Name        INTEGER,
+Vorname     Char(15),
+GebDatum    DATE,
+TodDatum    DATE,
+Werke       Char (100),
+Beruf       Char (20),
+Wohnort     Char (30),
+);
+
 CREATE Table tbl_07b_ausleihvorgang(
 AusleihNr INTEGER PRIMARY KEY,
 KundenNr INTEGER FOREIGN KEY,
