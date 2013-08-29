0ba115c22e906808dfda30e1e7afec423575b8da
Eric S. <Korni22@users.noreply.github.com>
2013-08-29 08:52:31 +0200

    Update 29.08.13 Bibliothek.sql

--- SQL/29.08.13 Bibliothek.sql
+++ SQL/29.08.13 Bibliothek.sql
@@ -1,4 +1,12 @@
- 
-use dblkap09
+--Übung(SQL-2 : DDL u. DML)
+USE DbLKap09; --Übungsdatenbabk auswählen
 
-create table tbl_03bkunden
+--*************************************************************************
+--Übungsaufgabe 2
+DROP TABLE tbl_ueArtikel07b
+
+CREATE TABLE tbl_ueArtikel08b(
+Nr  INTEGER		PRIMARY KEY	IDENTITY(100,10),
+Nam	CHAR(3),
+Anzahl	INTEGER,
+);	
